dd3a5819f910dacab836b831fba883d40cec7b38
Freddie Topping <freddietopping06@gmail.com>
2026-02-02 14:56:25 +0000

    commited system.h

---
+++ system.h
@@ -0,0 +1,20 @@
+#ifndef SYSTEM_H
+#define SYSTEM_H
+
+#include <vector>
+#include "box.h"
+#include "particle.h"
+
+class System {
+	public:
+		System();
+		int particleNumber;
+		double noiseStrength;
+		Box simulationBox;
+		std::vector<Particle> particles;
+
+		void updateRule()
+};
+
+#endif
+
