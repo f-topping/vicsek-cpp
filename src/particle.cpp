97f0bd24c39eb167c18cd5b6cb91d7368359db58
Freddie Topping <freddietopping06@gmail.com>
2026-02-09 15:19:23 +0000

    created particle file particle.cpp

---
+++ src/particle.cpp
@@ -0,0 +1,11 @@
+#include "particle.h"
+#include <iostream>
+
+
+Particle::Particle(){
+        this->x = 0;
+        this->y = 0;
+        this->theta = 0;
+	this->r = 1;
+        this->v = 0.5;
+}
